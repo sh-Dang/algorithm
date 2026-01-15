f324108c880e15f0785ba971a305d9203c3c7357
sh-Dang <henrys200632@gmail.com>
2026-01-15 17:39:22 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Main{
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        System.out.print(A+B);
+    }
+}
