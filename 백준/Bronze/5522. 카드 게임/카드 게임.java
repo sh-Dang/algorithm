d4f6a615a446b12994505e3f5d25973402735d61
sh-Dang <henrys200632@gmail.com>
2026-01-31 14:13:55 +0900

    [Bronze V] Title: 카드 게임, Time: 72 ms, Memory: 11480 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        
+        System.out.print(a+b+c+d+e);
+    }
+}
