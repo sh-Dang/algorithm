69dfda90d85b1289d25c733d41bee15630008673
sh-Dang <henrys200632@gmail.com>
2026-02-04 19:29:40 +0900

    [Bronze V] Title: 기찍 N, Time: 1044 ms, Memory: 40756 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for(int i=a;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
