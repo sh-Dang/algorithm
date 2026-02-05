e1abe32f14f766a323b01cc46aea85d7a30a2baf
sh-Dang <henrys200632@gmail.com>
2026-02-05 21:38:49 +0900

    [Bronze IV] Title: Archivist, Time: 68 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        switch(br.readLine()){
+            case "1995":
+                System.out.print("ITMO");
+                break;
+            case "1996":
+                System.out.print("SPbSU");
+                break;
+            case "1997":
+                System.out.print("SPbSU");
+                break;
+            case "1998":
+                System.out.print("ITMO");
+                break;
+            case "1999":
+                System.out.print("ITMO");
+                break;
+            case "2000":
+                System.out.print("SPbSU");
+                break;
+            case "2001":
+                System.out.print("ITMO");
+                break;
+            case "2002":
+                System.out.print("ITMO");
+                break;
+            case "2003":
+                System.out.print("ITMO");
+                break;
+            case "2004":
+                System.out.print("ITMO");
+                break;
+            case "2005":
+                System.out.print("ITMO");
+                break;
+            case "2006":
+                System.out.print("PetrSU, ITMO");
+                break;
+            case "2007":
+                System.out.print("SPbSU");
+                break;
+            case "2008":
+                System.out.print("SPbSU");
+                break;
+            case "2009":
+                System.out.print("ITMO");
+                break;
+            case "2010":
+                System.out.print("ITMO");
+                break;
+            case "2011":
+                System.out.print("ITMO");
+                break;
+            case "2012":
+                System.out.print("ITMO");
+                break;
+            case "2013":
+                System.out.print("SPbSU");
+                break;
+            case "2014":
+                System.out.print("ITMO");
+                break;
+            case "2015":
+                System.out.print("ITMO");
+                break;
+            case "2016":
+                System.out.print("ITMO");
+                break;
+            case "2017":
+                System.out.print("ITMO");
+                break;
+            case "2018":
+                System.out.print("SPbSU");
+                break;
+            case "2019":
+                System.out.print("ITMO");
+                break;
+        }
+    }
+}
