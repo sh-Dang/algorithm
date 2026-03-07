481d5f2a191cb5be9d43957923596eddd0f121d1
sh-Dang <henrys200632@gmail.com>
2026-03-07 16:10:42 +0900

    [Bronze IV] Title: 모음의 개수, Time: 60 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String s = br.readLine();
+            if(s.equals("#")) break;
+            int count = 0;
+            for(int i=0;i<s.length();i++){
+                char c = s.charAt(i);
+                if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
