7c4843bad224e61d612ea6fe0e2b15f31832e8f4
sh-Dang <henrys200632@gmail.com>
2026-03-23 23:52:54 +0900

    [Silver IV] Title: solved.ac, Time: 304 ms, Memory: 37448 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 들어오는 점수의 개수
+        
+        // n이 0이면 0출력하고 끝내기
+        if(n==0){
+            System.out.print(0);
+            return;
+        }
+        
+        int exceptedValue = (int) Math.round(n*0.15); // 제거할 점수의 개수 15% 반올림하는 함수 뭐더라 round?
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(arr); // 정렬하고
+        int sum = 0;
+        for(int i = exceptedValue;i<n-exceptedValue;i++){
+            sum += arr[i];
+        }
+        
+        int result = (int) Math.round((double) sum/(n-exceptedValue*2));
+        
+        System.out.print(result);
+    }
+}
