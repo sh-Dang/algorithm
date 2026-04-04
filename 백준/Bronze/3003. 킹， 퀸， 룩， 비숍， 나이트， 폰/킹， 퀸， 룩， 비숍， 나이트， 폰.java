53b8189b6396b586818c9e7c6f558c6970655daf
이세형 <henrys200632@gmail.com>
2026-04-04 23:51:18 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int king = Integer.parseInt(st.nextToken());
+        int queen = Integer.parseInt(st.nextToken());
+        int rook = Integer.parseInt(st.nextToken());
+        int bishop = Integer.parseInt(st.nextToken());
+        int knight = Integer.parseInt(st.nextToken());
+        int pawn = Integer.parseInt(st.nextToken());
+        
+        System.out.print((1 - king) + " ");
+        System.out.print((1 - queen) + " ");
+        System.out.print((2 - rook) + " ");
+        System.out.print((2 - bishop) + " ");
+        System.out.print((2 - knight) + " ");
+        System.out.print(8 - pawn);
+    }
+}
