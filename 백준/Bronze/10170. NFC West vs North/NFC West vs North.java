89a6998c2ad5ddd996c91cbda53b303a7c10c054
이세형 <henrys200632@gmail.com>
2026-04-06 19:38:06 +0900

    [Bronze V] Title: NFC West vs North, Time: 64 ms, Memory: 11324 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args)throws Exception{
+        StringBuilder sb = new StringBuilder();
+        sb.append("NFC West       W   L  T").append('\n');
+        sb.append("-----------------------").append('\n');
+        sb.append("Seattle        13  3  0").append('\n');
+        sb.append("San Francisco  12  4  0").append('\n');
+        sb.append("Arizona        10  6  0").append('\n');
+        sb.append("St. Louis      7   9  0").append('\n');
+        sb.append('\n');
+        sb.append("NFC North      W   L  T").append('\n');
+        sb.append("-----------------------").append('\n');
+        sb.append("Green Bay      8   7  1").append('\n');
+        sb.append("Chicago        8   8  0").append('\n');
+        sb.append("Detroit        7   9  0").append('\n');
+        sb.append("Minnesota      5  10  1").append('\n');
+        
+        System.out.print(sb);
+    }
+}
